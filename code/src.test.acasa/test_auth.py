e124c0a7b570fb61dd8f98a9d7b920bc4fd758a7
es2021uab-1494769 <pol.pagesl@e-campus.uab.cat>
2021-06-03 12:26:29 +0200

    Add files via upload

--- code/src.test.acasa/test_auth.py
+++ code/src.test.acasa/test_auth.py
@@ -4,6 +4,13 @@ from src.main.acasa.auth_service import AuthService
 from src.main.acasa.auth_user import AuthUser
 
 
+@pytest.fixture()
+def auth_server():
+    def _auth_server(service):
+        return AuthUser(service)
+    return _auth_server()
+
+
 class TestAuth:
 
     # 1
@@ -19,31 +26,33 @@ class TestAuth:
         assert check_auth is 'AuthURL'
 
     # 3
-    def test_given_an_authorization_when_request_connection_then_request_authentication_method_is_called(self) -> None:
-
-        result = True
-        assert result is True
+    def test_given_an_authorization_when_request_connection_then_request_authentication_method_is_called(self, service) -> None:
+        client_id = 'client_2'
+        mock_service = MagicMock()
+        auth_code = request_authorization(get_service(client_id))
+        auth_server(mock_service).connect(auth_code)
+        mock_service.request_authentication.assert_called_once_with(client_id, 'secret', auth_code, 'CallbackURL')
 
     # 4
     def test_given_an_authorization_when_request_connection_and_authentication_fails_then_return_connection_refused(self) -> None:
-
+        client_id = 'client_2'
         result = True
         assert result is True
 
     # 5
     def test_given_an_authorization_when_request_connection_and_authentication_works_then_return_connection_accepted(self) -> None:
-
+        client_id = 'client_2'
         result = True
         assert result is True
 
     # 6
     def test_given_an_authorization_when_request_connection_and_authentication_works_then_call_user_data_functions(self) -> None:
-
+        client_id = 'client_2'
         result = True
         assert result is True
 
     # 7
     def test_given_an_authorization_when_request_connection_and_authentication_fails_then_not_call_user_data_functions(self) -> None:
-
+        client_id = 'client_2'
         result = True
         assert result is True
